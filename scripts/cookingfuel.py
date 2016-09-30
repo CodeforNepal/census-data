0162b5abddd84dc5e1f3dab4ef59377cad5e26a3
Clifton McIntosh <cliff.mcintosh@gmail.com>
2016-09-29 20:32:20 -0500

    simplify script for generating cooking fuel data.

--- scripts/cookingfuel.py
+++ scripts/cookingfuel.py
@@ -4,33 +4,33 @@ import os
 import sys
 
 names_to_geo_ids = {
-    'Khotang': 'dstrct-13', 'Rautahat': 'dstrct-31', 'Dailekh': 'dstrct-63',
-    'Ramechhap': 'dstrct-18', 'Banke': 'dstrct-65', 'Sarlahi': 'dstrct-22',
-    'Myagdi': 'dstrct-49', 'Sindhuli': 'dstrct-19', 'Gulmi': 'dstrct-45',
-    'Saptari': 'dstrct-15', 'Parsa': 'dstrct-33', 'Rasuwa': 'dstrct-28',
-    'Salyan': 'dstrct-61', 'Rupandehi': 'dstrct-44', 'Mugu': 'dstrct-53',
-    'Bajura': 'dstrct-67', 'Dhankuta': 'dstrct-07', 'Dang': 'dstrct-60',
-    'Kathmandu': 'dstrct-27', 'Sankhuwasabha': 'dstrct-05',
-    'Solukhumbu': 'dstrct-11', 'Doti': 'dstrct-70',
-    'Arghakhanchi': 'dstrct-46', 'Baglung': 'dstrct-51',
-    'Bhojpur': 'dstrct-06', 'Dhanusha': 'dstrct-20', 'Panchthar': 'dstrct-02',
-    'Kalikot': 'dstrct-55', 'Tahanun': 'dstrct-38', 'Bardiya': 'dstrct-66',
-    'Lalitpur': 'dstrct-26', 'Humla': 'dstrct-56', 'Kaski': 'dstrct-40',
-    'Syangja': 'dstrct-41', 'Dadeldhura': 'dstrct-74', 'Dhading': 'dstrct-30',
-    'Pyuthan': 'dstrct-59', 'Taplejung': 'dstrct-01', 'Rolpa': 'dstrct-58',
-    'Bhaktapur': 'dstrct-25', 'Lamjung': 'dstrct-37', 'Sunsari': 'dstrct-10',
-    'Kapilbastu': 'dstrct-47', 'Kanchanpur': 'dstrct-75',
-    'Kailali': 'dstrct-71', 'Sindhupalchowk': 'dstrct-23',
-    'Jumla': 'dstrct-54', 'Morang': 'dstrct-09', 'Dolpa': 'dstrct-52',
-    'Surkhet': 'dstrct-64', 'Siraha': 'dstrct-16', 'Nawalparasi': 'dstrct-42',
-    'Chitwan': 'dstrct-35', 'Jhapa': 'dstrct-04', 'Baitadi': 'dstrct-73',
-    'Achham': 'dstrct-68', 'Makawanpur': 'dstrct-34', 'Bara': 'dstrct-32',
-    'Okhaldhunga': 'dstrct-12', 'Rukum': 'dstrct-57', 'Darchula': 'dstrct-72',
-    'Tehrathum': 'dstrct-08', 'Nuwakot': 'dstrct-29', 'Bajhang': 'dstrct-69',
-    'Mustang': 'dstrct-48', 'Parbat': 'dstrct-50', 'Udayapur': 'dstrct-14',
-    'Illam': 'dstrct-03', 'Manang': 'dstrct-39', 'Palpa': 'dstrct-43',
-    'Dolakha': 'dstrct-17', 'Jajarkot': 'dstrct-62', 'Mahottari': 'dstrct-21',
-    'Kavre': 'dstrct-24', 'Gorkha': 'dstrct-36'
+    'Khotang': '13', 'Rautahat': '31', 'Dailekh': '63',
+    'Ramechhap': '18', 'Banke': '65', 'Sarlahi': '22',
+    'Myagdi': '49', 'Sindhuli': '19', 'Gulmi': '45',
+    'Saptari': '15', 'Parsa': '33', 'Rasuwa': '28',
+    'Salyan': '61', 'Rupandehi': '44', 'Mugu': '53',
+    'Bajura': '67', 'Dhankuta': '07', 'Dang': '60',
+    'Kathmandu': '27', 'Sankhuwasabha': '05',
+    'Solukhumbu': '11', 'Doti': '70',
+    'Arghakhanchi': '46', 'Baglung': '51',
+    'Bhojpur': '06', 'Dhanusha': '20', 'Panchthar': '02',
+    'Kalikot': '55', 'Tahanun': '38', 'Bardiya': '66',
+    'Lalitpur': '26', 'Humla': '56', 'Kaski': '40',
+    'Syangja': '41', 'Dadeldhura': '74', 'Dhading': '30',
+    'Pyuthan': '59', 'Taplejung': '01', 'Rolpa': '58',
+    'Bhaktapur': '25', 'Lamjung': '37', 'Sunsari': '10',
+    'Kapilbastu': '47', 'Kanchanpur': '75',
+    'Kailali': '71', 'Sindhupalchowk': '23',
+    'Jumla': '54', 'Morang': '09', 'Dolpa': '52',
+    'Surkhet': '64', 'Siraha': '16', 'Nawalparasi': '42',
+    'Chitwan': '35', 'Jhapa': '04', 'Baitadi': '73',
+    'Achham': '68', 'Makawanpur': '34', 'Bara': '32',
+    'Okhaldhunga': '12', 'Rukum': '57', 'Darchula': '72',
+    'Tehrathum': '08', 'Nuwakot': '29', 'Bajhang': '69',
+    'Mustang': '48', 'Parbat': '50', 'Udayapur': '14',
+    'Illam': '03', 'Manang': '39', 'Palpa': '43',
+    'Dolakha': '17', 'Jajarkot': '62', 'Mahottari': '21',
+    'Kavre': '24', 'Gorkha': '36'
 }
     
 
@@ -48,41 +48,28 @@ def cookingfuelcsv(districtsdir, outputfile):
     csvfilenames = list(map(build_csv_location,
                             get_immediate_subdirectories(districtsdir)))
 
-    district_dict = {}
+    all_rows = []
+    national_totals = {}
     for cookingfile in csvfilenames:
         district_geoid = names_to_geo_ids[cookingfile.split('/')[-2]]
         with open(cookingfile, 'r') as cooking:
             all = cooking.readlines()
             headers = all[0].split(',')[1:]
             totals = all[len(all) - 1].split(',')[1:]
-            district_vals = {}
 
             for idx, header in enumerate(headers):
-                district_vals[header.strip(' \t\n\r')] = \
-                    int(totals[idx].strip(' \t\n\r'))
-
-            district_dict[district_geoid] = district_vals
-
-    all_rows = []
-    national_totals = {}
-    for key, value in district_dict.items():
-        for fuel in value.keys():
-            if fuel == 'LPG':
-                fuel_name = fuel
-            else:
-                fuel_name = fuel.lower().replace('_', ' ').capitalize()
-
-            all_rows.append({
-                'geo_level': 'district',
-                'geo_code': key,
-                'main type of cooking fuel': fuel_name,
-                'total': value[fuel]
-            })
-            if fuel_name in national_totals:
-                national_totals[fuel_name] = national_totals[fuel_name] \
-                                                + value[fuel]
-            else:
-                national_totals[fuel_name] = value[fuel]
+                fuel_name = header.strip(' \t\n\r')
+                district_total = int(totals[idx].strip(' \t\n\r'))
+                all_rows.append({
+                    'geo_level': 'district',
+                    'geo_code': district_geoid,
+                    'main type of cooking fuel': fuel_name,
+                    'total': district_total
+                })
+                if fuel_name in national_totals:
+                    national_totals[fuel_name] += district_total
+                else:
+                    national_totals[fuel_name] = district_total
 
     for key, value in national_totals.items():
         all_rows.append({
@@ -93,7 +80,8 @@ def cookingfuelcsv(districtsdir, outputfile):
         })
 
     with open(outputfile, 'w') as csvout:
-        fieldnames = all_rows[0].keys()
+        fieldnames = ['geo_code', 'geo_level',
+                      'main type of cooking fuel', 'total']
         writer = csv.DictWriter(csvout, fieldnames=fieldnames)
         writer.writeheader()
         for row in all_rows:
